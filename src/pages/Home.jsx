981046a8c753489d2aabbfc54143aae952afd492
chiranjeeb-nvest <chiranjeeb@nvestbank.com>
2020-09-28 15:50:57 +0530

    Tracking Page Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Card, Col, Input, Row } from "antd";
+import { Alert, Avatar, Button, Card, Col, Input, message, Row } from "antd";
 import Axios from "axios";
 import Loading from "../lotties/LoadingAnimation";
 
@@ -18,21 +18,15 @@ const Home = () => {
   const [showInput, setShowInput] = useState(false);
   const [lottiLoading, setLottiLoading] = useState(false);
   const [showTracking, setShowTracking] = useState(false);
+  const [trackingData, setTrackingData] = useState([]);
+  const [myError, setMyError] = useState(false);
+  const [checkNumber, setCheckNumber] = useState("");
 
   useEffect(() => {
     if (showInput) {
       chkNumberRef.current.focus();
     }
   }, [showInput]);
-
-  useEffect(() => {
-    if (lottiLoading) {
-      setTimeout(() => {
-        setLottiLoading(false);
-        setShowTracking(true);
-      }, 2000);
-    }
-  }, [lottiLoading]);
 
   useEffect(() => {
     Axios.get(
@@ -48,91 +42,343 @@ const Home = () => {
     });
   }, []);
 
+  const getTrackingData = () => {
+    setLottiLoading(true);
+    console.log(chkNumberRef.current.state.value);
+    Axios.get(
+      `https://comms.globalxchange.com/coin/vault/service/blockcheck/request/get?_id=${checkNumber}`
+    ).then((res) => {
+      if (res.data.status) {
+        setTrackingData(res.data.bcrs);
+        setLottiLoading(false);
+        setShowTracking(true);
+      } else {
+        setLottiLoading(false);
+        // message.error("Wrong Check Number");
+        setMyError(true);
+        setCheckNumber("");
+        chkNumberRef.current.focus();
+        setTimeout(() => {
+          setMyError(false);
+        }, 1500);
+      }
+    });
+  };
+
+  const showTrackingData = () => {
+    console.log(trackingData[0], "trackingdata");
+    if (trackingData.length > 0) {
+      return (
+        <>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+              padding: "50px",
+              paddingBottom: "10px",
+            }}
+          >
+            <img
+              src={require("../images/back.svg")}
+              alt=""
+              width="30px"
+              onClick={(e) => setShowTracking(false)}
+            />
+            &nbsp;&nbsp;
+            <div>
+              <img
+                src={require("../images/logo_blue.svg")}
+                alt=""
+                width="250px"
+              />
+            </div>
+          </div>
+
+          <div
+            style={{
+              fontSize: "20px",
+              color: "#4D4D4D",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            In Transit
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "82vh",
+            }}
+          >
+            <div
+              style={{ padding: "0px 20px", paddingTop: "30px", zIndex: 99 }}
+            >
+              <Card bodyStyle={{ display: "flex", alignItems: "center" }}>
+                <Avatar size="large" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <div style={{ color: "#186AB4" }}>
+                  <div style={{ fontSize: "17px", fontWeight: "bold" }}>
+                    Initiated By {trackingData[0].initiator_name}
+                  </div>
+                  <small>{trackingData[0].date}</small>
+                </div>
+              </Card>
+              <br />
+              <Card bodyStyle={{ display: "flex", alignItems: "center" }}>
+                <Avatar size="large" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <div style={{ color: "#186AB4" }}>
+                  <div style={{ fontSize: "17px", fontWeight: "bold" }}>
+                    Delivered To {trackingData[0].endUser_name}
+                  </div>
+                  <small>{trackingData[0].date}</small>
+                </div>
+              </Card>
+              <br />
+              <Card bodyStyle={{ display: "flex", alignItems: "center" }}>
+                <Avatar size="large" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <div style={{ color: "#186AB4" }}>
+                  <div style={{ fontSize: "17px", fontWeight: "bold" }}>
+                    Seen By {trackingData[0].endUser_name}
+                  </div>
+                  <small>{trackingData[0].date}</small>
+                </div>
+              </Card>
+            </div>
+            <div style={{ padding: "0px 20px", zIndex: 99 }}>
+              <Card bodyStyle={{ display: "flex", alignItems: "center" }}>
+                <Avatar size="large" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <div style={{ color: "#186AB4" }}>
+                  <div style={{ fontSize: "17px", fontWeight: "bold" }}>
+                    {isCash
+                      ? `Cashed By ${trackingData[0].endUser_name}`
+                      : `Signed by ${trackingData[0].endUser_name}`}
+                  </div>
+                  <small>{trackingData[0].date}</small>
+                </div>
+              </Card>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  padding: "20px 0px",
+                }}
+              >
+                <div
+                  style={{
+                    height: "50px",
+                    border: "solid 0.5px #186AB4",
+                    width: "100%",
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                    color: "#186AB4",
+                    cursor: "pointer",
+                    background: "white",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  Share
+                </div>
+                &nbsp;&nbsp; &nbsp;&nbsp;
+                <div
+                  style={{
+                    height: "50px",
+                    border: "solid 0.5px #186AB4",
+                    width: "100%",
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                    color: "white",
+
+                    cursor: "pointer",
+                    background: "#186AB4",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  Audit
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              position: "fixed",
+              left: "15vw",
+              top: "25vh",
+              bottom: "10vh",
+              background: "#186AB4",
+              height: "55vh",
+              width: "5px",
+              zIndex: 0,
+            }}
+          >
+            &nbsp;
+          </div>
+        </>
+      );
+    }
+  };
+
+  const paste = async (input) => {
+    chkNumberRef.current.focus();
+    const text = await navigator.clipboard.readText();
+    setCheckNumber(text);
+    // chkNumberRef.current.state.value = text;
+    // input.value = text;
+  };
+
+  useEffect(() => {
+    if (trackingData.length > 0) {
+      showTrackingData();
+    }
+  }, [trackingData]);
+
   const handleShowSection = () => {
     if (lottiLoading) {
       return <Loading />;
     } else {
       if (showInput && !showTracking) {
         return (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              height: "100vh",
-            }}
-          >
+          <>
             <div
               style={{
-                textAlign: "center",
-                padding: "0px 50px",
-                paddingTop: "50px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+                padding: "50px",
+                paddingBottom: "10px",
               }}
             >
               <img
-                src={require("../images/logo_blue.svg")}
+                src={require("../images/back.svg")}
                 alt=""
-                width="250px"
+                width="30px"
+                onClick={(e) => setShowInput(false)}
               />
-              <p
-                style={{
-                  color: "#186AB4",
-                  padding: "20px",
-                  textAlign: "center",
-                  fontSize: "18px",
-                }}
-              >
-                Enter The Check Number To Open Up The CheckBook.
-              </p>
-            </div>
-
-            <Row style={{ width: "100%", padding: "30px" }}>
-              <Col
-                span={16}
-                //   style={{ border: "solid 0.5px #EBEBEB", padding: "20px" }}
-              >
-                <Input
-                  ref={chkNumberRef}
-                  // value={slideName}
-                  // onChange={(e) => setSlideName(e.target.value)}
-                  placeholder="Enter Check Number"
-                  style={{
-                    borderRadius: "0px",
-                    border: "none !important",
-                    outline: "none !important",
-                    height: "54px",
-                  }}
+              &nbsp;&nbsp;
+              <div>
+                <img
+                  src={require("../images/logo_blue.svg")}
+                  alt=""
+                  width="250px"
                 />
-              </Col>
-              <Col
-                span={4}
+              </div>
+            </div>
+            <p
+              style={{
+                color: "#186AB4",
+                padding: "20px 60px",
+                textAlign: "center",
+                fontSize: "18px",
+              }}
+            >
+              Enter The Check Number To Open Up The CheckBook.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                height: "100vh",
+              }}
+            >
+              {/* <div
                 style={{
-                  border: "solid 1px #EBEBEB",
-                  borderWidth: "1px 1px 1px 0px",
-                  height: "54px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
+                  textAlign: "center",
+                  padding: "0px 50px",
+                  paddingTop: "50px",
                 }}
               >
-                <img src={require("../images/paste.svg")} alt="" width="20px" />
-              </Col>
-              <Col
-                onClick={(e) => setLottiLoading(true)}
-                span={4}
-                style={{
-                  border: "solid 0.5px #EBEBEB",
-                  height: "54px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "#186AB4",
-                }}
-              >
-                <img src={require("../images/go.svg")} alt="" width="20px" />
-              </Col>
-            </Row>
-          </div>
+                <img
+                  src={require("../images/logo_blue.svg")}
+                  alt=""
+                  width="250px"
+                />
+                
+              </div> */}
+
+              <Row style={{ width: "100%", padding: "30px" }}>
+                <Col
+                  span={16}
+                  //   style={{ border: "solid 0.5px #EBEBEB", padding: "20px" }}
+                >
+                  <Input
+                    ref={chkNumberRef}
+                    value={checkNumber}
+                    onChange={(e) => setCheckNumber(e.target.value)}
+                    placeholder="Enter Check Number"
+                    style={{
+                      borderRadius: "0px",
+                      border: "none !important",
+                      outline: "none !important",
+                      height: "54px",
+                    }}
+                  />
+                </Col>
+                <Col
+                  onClick={paste}
+                  span={4}
+                  style={{
+                    border: "solid 1px #EBEBEB",
+                    borderWidth: "1px 1px 1px 0px",
+                    height: "54px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img
+                    src={require("../images/paste.svg")}
+                    alt=""
+                    width="20px"
+                  />
+                </Col>
+                <Col
+                  onClick={getTrackingData}
+                  span={4}
+                  style={{
+                    border: "solid 0.5px #EBEBEB",
+                    height: "54px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "#186AB4",
+                  }}
+                >
+                  <img src={require("../images/go.svg")} alt="" width="20px" />
+                </Col>
+              </Row>
+              {myError ? (
+                <div
+                  style={
+                    {
+                      // padding: "10px 30px",
+                      // color: "red",
+                      // fontWeight: "bold",
+                    }
+                  }
+                >
+                  <Alert
+                    message="Please Enter a Valid Check Number"
+                    type="error"
+                    showIcon
+                  />
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </>
         );
       } else if (!showInput && !showTracking) {
         return (
@@ -297,11 +543,7 @@ const Home = () => {
           </>
         );
       } else if (showTracking) {
-        return (
-          <div>
-            <p>this is tracking...</p>
-          </div>
-        );
+        return showTrackingData();
       }
     }
   };
